037986208471982b9886c0fb5b64915c03cae85e
mslaramie <85331181+mslaramie@users.noreply.github.com>
2022-03-20 12:40:13 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# hackathon-project
+### Group Members
+  - Michael Laramie
+  - Jonathan Hamstra
+  - An Dang
+  - Caleb Flegel
+---
+### Theme/Prompt
+  1. Meeting New People
+  2. Finding New Opportunities
+  3. Making New Connections
+  4. Breaking out of Your Bubble
+  5. Connecting Communities Through Technology
+### Judging Categories
+  1. Theme Fit
+  2. Project Completion
+  3. Innovation Factor
+  4. Presentation Quality
+---
+### Project Description
+Our project will be an Event Tracker that will enable students at Whitworth to create events via a picture/map of the campus with X and Y coordinates to map where everything is. The events will be color coded ideally, with Red being an official school event (football game, primetime, etc), Yellow being studying and academia related events, and green being unofficial and casual events (Smash Bros tournament). In the app the user will click on the area where the event will be held, then will go through a series of prompts to add information to what the event is. From there other users in the app will be able to see those events and choose to attend the events, after which a list will be updated to include either their name, or a counter displaying how mnay will be there.
+
+**Feature Specifications**
+  1. Picture of Map for users to view.
+  2. User can add events to map after filling out event information.
+  3. List of people going to X event OR counter of X people going to event.
+  4. Not limited to official whitworth events only (like HFS).
+  5. Color Coded Events, Red - Official, Yellow - Academia, Green - Unofficial
+  6. Campus Wide, enrolled students only.
+  7. Optional: Nice User Interface
+  
+**Improvements**
+  1. Display how many people are going to X event.
+  2. UI Improvements
+  3. Less ballistic spelling syntax
+---
---
+++ main.py
@@ -0,0 +1,222 @@
+from ctypes import pointer
+from dis import dis
+from multiprocessing.dummy import Array
+from operator import add, truediv
+from turtle import screensize, width
+from typing import List
+from venv import create
+import pygame, sys
+import mysql.connector
+from tkinter import*
+pygame.init()
+myfont = pygame.font.SysFont("monospace", 15)
+text = ""
+file = pygame.image.load(r'./assets/Yellow.png')
+eventype = ""
+pull_id = 0
+# --------------------------------------------
+# Works Cited/Referenced
+# Code for displaying an image via Pygame: 
+# https://www.geeksforgeeks.org/python-display-images-with-pygame/#:~:text=There%20are%20four%20basic%20steps,drawn%20on%20it%2C%20using%20image. 
+# Code for DB Connection Stuff:
+# https://www.w3schools.com/python/python_mysql_insert.asp 
+# Tkinter Input Window Popup:
+# https://python-course.eu/tkinter/entry-widgets-in-tkinter.php 
+#---------------------------------------------
+mydb = mysql.connector.connect(
+  host="192.9.227.213",
+  user="hackathon",
+  password="hackathon123",
+  database = "hackathon1"
+)
+#Create event will be used following a series of user inputs to add the corresponding information to the database.
+def createEvent(date, time, name, descr, location, Ev_type, mapx, mapy): # People count and ID will be auto updated.
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO Event_Info (Date, Time, Event_Name, Event_Descr, Event_Location, Event_Type, Map_X, Map_Y) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (date, time, name, descr, location, Ev_type, mapx, mapy)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    initilize_arrays()
+    update_screen()
+# Testing create event here:
+# createEvent("2022-01-17", "5:23:00", "Elden Ring Party", "Failing to kill Godrick for 90 hours", "Ninth Circle of Hell", "Green", 340, 340)
+# createEvent("2022-02-28", "4:23:00", "Pizza eating comp", "Making bad decisions and also diabeetus", "The Tunnels", "Yellow", 1020, 400)
+# createEvent("2022-03-05", "1:30:00", "Doing coke in the hub", "Crackhead", "HUB", "Red", 1200, 1200)
+def initilize_arrays():
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM Event_Info"
+    mycursor.execute(sql)
+    rows = mycursor.fetchall()
+    global ID_Coords
+    ID_Coords = [None] * len(rows)
+    i = 0
+    for row in rows:
+        ID_Coords[i] = (row[0], row[6], row[8], row[9])
+        i += 1
+# Dimensions of image to display
+display_surface = pygame.display.set_mode((1920, 1080))
+  
+# Var with white RGB
+white = (255, 255, 255)
+black = (0, 0, 0)    
+themeGreen = (64, 84, 39)
+themeWhite = (240, 241, 238)
+eventGreen = (105, 153, 93)
+eventYellow = (250, 185, 21)
+eventRed = (152, 71, 63)
+
+    
+# Assigning names to sprites
+pygame.display.set_caption('Image')
+pygame.display.set_caption('redImg')
+pygame.display.set_caption('greenImg')
+pygame.display.set_caption('yellowImg')
+  
+# Make surface for the sprites to display onto
+map_background = pygame.image.load(r'./assets/campus-map.jpg')
+official_icon = pygame.image.load(r'./assets/Red Event.png')
+unofficial_icon = pygame.image.load(r'./assets/Green Event.png')
+neutral_icon = pygame.image.load(r'./assets/Yellow Event.png')
+
+#Adding the sprites for the buttons
+addEventButton = pygame.image.load(r'./assets/AddEvent.png')
+clearEventsButton = pygame.image.load(r'./assets/ClearEvents.png')
+quitButton = pygame.image.load(r'./assets/QuitButton.png')
+
+#Locations for the buttons
+addEventButtonLocation = (1590, 5)
+clearEventsButtonLocation = (1700, 5)
+quitButtonLocation = (1810, 5)
+
+# Print the map
+display_surface.blit(map_background, (-384, -170))
+#------TKINTER WINDOW POPUP SETUP-------# This was referenced
+# Array that will store user input
+userInput = [None] * 6
+# Fields that will be displayed to the user
+fields = 'Date(YYYY-MM-DD', 'Time(HH:MM:SS)', 'Event Name', 'Description', 'Location', 'Type'
+
+# Activates the popup on use, prompting the user for input
+def popUp(mapx, mapy):
+    root = Tk() # Core of Tk() is activated
+    ents = makeform(root, fields) # make the window
+    b1 = Button(root, text='Log', # make log button, if pressed insert info into array
+                  command=(lambda e=ents: fetch(e, mapx, mapy)))
+    b1.pack(side=LEFT, padx=5, pady=5) # padding
+    master = Tk()
+    master.mainloop()
+
+def fetch(entries, mapx, mapy): # get the entries, insert into array via for loop
+    i = 0
+    for entry in entries:
+        text = entry[1].get()
+        userInput[i] = text
+        i += 1
+    userInput.append(mapx)
+    userInput.append(mapy)
+    print(userInput)
+    createEvent(userInput[0], userInput[1], userInput[2], userInput[3], userInput[4], userInput[5], mapx, mapy)
+def makeform(root, fields):
+    entries = []
+    for field in fields:
+        row = Frame(root)
+        lab = Label(row, width=15, text=field, anchor='w')
+        ent = Entry(row)
+        row.pack(side=TOP, fill=X, padx=5, pady=5)
+        lab.pack(side=LEFT)
+        ent.pack(side=RIGHT, expand=YES, fill=X)
+        entries.append((field, ent))
+    return entries
+#---------------------------------------
+
+def update_screen():
+    display_surface.blit(map_background, (-384, -170))
+    for i in ID_Coords:
+        event_type = str(i[1])
+        if event_type == "Green":
+            file = unofficial_icon
+        elif event_type == "Red":
+            file = official_icon
+        else:
+            file = neutral_icon
+        display_surface.blit(file, (i[2] - 33, i[3] - 35))
+    
+    #Adding the buttons
+    display_surface.blit(quitButton, (quitButtonLocation))
+    display_surface.blit(clearEventsButton, (clearEventsButtonLocation))
+    display_surface.blit(addEventButton, (addEventButtonLocation))
+        
+def print_result(ID):
+    master = Tk()
+    mycursor = mydb.cursor()
+    sql = f"SELECT * FROM Event_Info WHERE ID={ID}"
+    mycursor.execute(sql)
+    result = mycursor.fetchone()
+    output = ["Date: %s\n" % result[1]]
+    output.append("Time: %s\n" % result[2])
+    output.append("Title: %s\n" % result[3])
+    output.append("Location: %s\n" % result[5])
+    output.append("Description: %s" % result[4])
+    msg = Message(master, text = "".join(output))
+    msg.config(bg='white', font=('times', 18, 'italic'), aspect = 200, relief = RAISED)
+    msg.pack()
+    mainloop()
+
+initilize_arrays()
+update_screen()
+while True :  
+    # Loop keeps running until closing
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # deactivates the pygame library and quit program
+            pygame.quit()
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                while 1:
+                    pygame.event.clear()
+                    event = pygame.event.wait()
+                    if event.type == pygame.KEYUP:
+                        key_in = event.key
+                        if key_in == pygame.K_RETURN:
+                            break
+                        elif key_in == pygame.K_SPACE:
+                            text += " "
+                        elif key_in == pygame.K_BACKSPACE:
+                            text = text[0:-1]
+                        else:
+                            temp = pygame.key.name(key_in)
+                            text += temp
+                    text_display = myfont.render(text, 1, (255,255,0))
+                    display_surface.blit(text_display, (500, 500))
+                    pygame.display.update()
+            elif event.key == pygame.K_t:
+                popUp()
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            temp = list(pos)
+            if pos[0] >= addEventButtonLocation[0] and pos[0] <= addEventButtonLocation[0] + 105 and pos[1] >= addEventButtonLocation[1] and pos[1] <= addEventButtonLocation[1]+56:
+                wait = True
+                while wait == True:
+                    ev = pygame.event.get()
+                    for event in ev:
+                        if event.type == pygame.MOUSEBUTTONUP:
+                            newpos = pygame.mouse.get_pos()
+                            mapx = newpos[0]
+                            mapy = newpos[1]
+                            popUp(mapx, mapy)
+                            wait = False
+            elif pos[0] >= quitButtonLocation[0] and pos[0] <= quitButtonLocation[0] + 105 and pos[1] >= quitButtonLocation[1] and pos[1] <= quitButton[1]+56:
+                pygame.quit()
+            else:
+                for i in ID_Coords:
+                    if abs(i[2] - temp[0]) < 35:
+                        for n in ID_Coords:
+                            if abs(n[3] - temp[1]) < 35:
+                                pull_id = n[0]
+                if pull_id != 0:
+                    print_result(pull_id)   
+                    pull_id = 0
+        # Draws the surface object to the screen.  
+    pygame.display.update()
